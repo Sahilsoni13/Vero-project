73279404175c89103fe2a9f5bbfc6d474dd41699
sahil soni <sonisahil1018@gmail.com>
2024-01-25 12:34:46 +0530

    banner mostly fixed

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,53 +1,80 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Link,Outlet } from 'react-router-dom'
-import Logomain from "../images/logomain.png"
+import React, { useEffect, useRef, useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import Logomain from "../images/logomain.png";
 import Search from "../images/search.png";
 import Profile from "../images/profile.png";
 import Cart from "../images/cart.png";
 const Header = () => {
- let bg = (e)=>{
-    const navbar = document.getElementById("nav");
-    navbar.classList.toggle("addme")
- } 
-let changeicon = function(){
-  let icon = document.querySelector(".fa-solid")
-  icon.classList.toggle("fa-xmark")
-}
-const mainfunc = ()=>{
-  bg();
-  changeicon(this)  
-}
-  return (  
+  const [open, setopen] = useState(false);
+  return (
     <header className="header">
-    <div className="container">
-      <div className="hide-logo "  >
-      <img src={Logomain} />
-      {/* <img src={Burger} alt="" className='burger' /> */}
-      <i  id='hamburger' className="fa-solid fa-bars" onClick={mainfunc}  ></i>
-      </div>
-        <nav className="navigation flex" id='nav'>
-            <div className="nav-logo">
-             <img src={Logomain} />
-            </div>
-            <div className="nav-menu flex">
-             <li><Link to="#" className=' navlinks'>Home</Link></li>
-             <li><Link to="#" className=' navlinks'>About Us</Link></li>
-             <li><Link to="#" className=' navlinks'>About Our Brace</Link></li>
-             <li><Link to="#" className=' navlinks'>Shop</Link></li>
-             <li><Link to="#" className=' navlinks'>Instructions</Link></li>
-             <li><Link to="#" className=' navlinks'>FAQ</Link></li>
-             <li><Link to="#" className=' navlinks'>Rehab Resources</Link></li>
-             <li><Link to="#" className=' navlinks'>Contact</Link></li>
-            </div>
-            <div className="nav-icons flex">
-             <img src={Search} />
-             <img src={Profile} className='mid-margin' />
-             <img src={Cart} />
-            </div>
-        </nav>
-        <Outlet/>
+      <div className="container">
+        <div className="hide-logo ">
+          <img src={Logomain} />
+          <i
+            id="hamburger"
+            onClick={() => setopen(!open)}
+            className={open === true ? "fa-solid fa-xmark" : "fa-solid fa-bars"}
+          ></i>
         </div>
+        <nav
+          className={open === true ? " navigation addme" : "navigation"}
+          id="nav"
+        >
+          <div className="nav-logo">
+            <img src={Logomain} />
+          </div>
+          <div className="nav-menu flex">
+            <li>
+              <Link to="#" className=" navlinks">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className=" navlinks">
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className=" navlinks">
+                About Our Brace
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className=" navlinks">
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className=" navlinks">
+                Instructions
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className=" navlinks">
+                FAQ
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className=" navlinks">
+                Rehab Resources
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className=" navlinks">
+                Contact
+              </Link>
+            </li>
+          </div>
+          <div className="nav-icons flex">
+            <img src={Search} />
+            <img src={Profile} className="mid-margin" />
+            <img src={Cart} />
+          </div>
+        </nav>
+        <Outlet />
+      </div>
     </header>
-  )
-}
+  );
+};
 export default Header;
--- src/components/home/banner.js
+++ src/components/home/banner.js
@@ -3,64 +3,76 @@ import Ankle from "../images/Ankle.png";
 import img1 from "../images/img1.png";
 import img2 from "../images/img2.png";
 import img3 from "../images/img3.png";
-import Fade from 'react-reveal/Fade';
+// import Bannerbg from "../images/banner-bg.png"
 const Banner = () => {
   return (
     <>
       <section className="banner-section">
         <div className="container">
-        <div className="banner-main">
-          <div className="banner-left">
-          <h1 className="banner-heading common-bg-midblue">
-              Your Path to Enhanced Recovery and Support
-            </h1>
-            <p className="banner-left-p common-bg-midblue mid-p-bio">
-              The Vero Ankle™ brace — Designed by leading ankle surgeons for
-              athletes,weekend warriors, injury recovery, and those looking for
-              extra ankle support for everyday activities.
-            </p>
-            <button type="button " className="solid-capsules common-white">Shop Now</button>
+          <div className="banner-main">
+            <div className="banner-left">
+              <h1 className="banner-heading common-bg-midblue">
+                Your Path to Enhanced Recovery and Support
+              </h1>
+              <p className="banner-left-p common-bg-midblue mid-p-bio">
+                The Vero Ankle™ brace — Designed by leading ankle surgeons for
+                athletes,weekend warriors, injury recovery, and those looking
+                for extra ankle support for everyday activities.
+              </p>
+              <button type="button " className="solid-capsules common-white">
+                Shop Now
+              </button>
+            </div>
+            <div className="banner-right">
+              <div className="abc">
+                <img src={Ankle} alt="" className="bannerimg" />
+                <div className="def" >
+                  <div className=" def1 expert expert-1">
+                    <div className="ex-left">
+                      <img src={img1} alt="" />
+                    </div>
+                    <div className="ex-right">
+                      <h3 className="expert-heading common-bg-blue ">
+                        Designed by experts
+                      </h3>
+                      <p className="expert-bio common-bg-blue ">
+                        Designed by a team with 100+ years of combined expertise
+                        in foot and ankle specialization.
+                      </p>
+                    </div>
+                  </div>
+                  <div className=" def2 expert expert-middle">
+                    <div className="ex-left">
+                      <img src={img2} alt="" />
+                    </div>
+                    <div className="ex-right">
+                      <h3 className="expert-heading common-bg-blue ">
+                        From Injury To Recovery
+                      </h3>
+                      <p className="expert-bio common-bg-blue ">
+                        Our Brace is tailored to support the healing process
+                        from the initial injury through to full recovery
+                      </p>
+                    </div>
+                  </div>
+                  <div className=" def3 expert expert-3">
+                    <div className="ex-left">
+                      <img src={img3} alt="" />
+                    </div>
+                    <div className="ex-right">
+                      <h3 className="expert-heading common-bg-blue ">
+                        Five Star Criteria Design
+                      </h3>
+                      <p className="expert-bio common-bg-blue ">
+                        Meets 5-star ankle support criteria: comfort, fit,
+                        user-friendliness, low profile, and flexibility.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="banner-right">
-            <img src={Ankle} alt="" className="bannerimg" />
-            <div className="expert expert-1">
-              <div className="ex-left">
-                <img src={img1} alt="" />
-              </div>
-              <div className="ex-right">
-                <h3 className="expert-heading common-bg-blue ">Designed by experts</h3>
-                <p className="expert-bio common-bg-blue ">
-                  Designed by a team with 100+ years of combined expertise in
-                  foot and ankle specialization.
-                </p>
-              </div>
-            </div>
-            <div className="expert expert-middle">
-              <div className="ex-left">
-                <img src={img2} alt="" />
-              </div>
-              <div className="ex-right">
-                <h3 className="expert-heading common-bg-blue ">From Injury To Recovery</h3>
-                <p className="expert-bio common-bg-blue ">
-                  Our Brace is tailored to support the healing process from the
-                  initial injury through to full recovery
-                </p>
-              </div>
-            </div>
-            <div className="expert expert-3">
-              <div className="ex-left">
-                <img src={img3} alt="" />
-              </div>
-              <div className="ex-right">
-                <h3 className="expert-heading common-bg-blue ">Five Star Criteria Design</h3>
-                <p className="expert-bio common-bg-blue ">
-                  Meets 5-star ankle support criteria: comfort, fit,
-                  user-friendliness, low profile, and flexibility.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
         </div>
         <div className="infinte">
           <h1>Comfortable</h1>
@@ -74,7 +86,7 @@ const Banner = () => {
           <h1>Flexible</h1>
           <h1>Supportive</h1>
         </div>
-        
+        {/* <img src={Bannerbg} alt="" /> */}
       </section>
     </>
   );
